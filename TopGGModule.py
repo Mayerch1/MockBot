29faf6d021c8f545731abc6368399a09af3e4979
Christian <it@cj-mayer.de>
2021-04-12 15:33:11 +0200

    use manual server count posting

--- TopGGModule.py
+++ TopGGModule.py
@@ -1,59 +1,44 @@
 import os
 import dbl
-import web
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
-import _thread
 
-from flask import Flask, request
-from waitress import serve
 
-from consts import Consts
-from lib.tinyConnector import TinyConnector
 
 class TopGG(commands.Cog):
 
-    app = Flask(__name__)
-    user_votes = {}
-    secret = None
+    def __init__(self, bot):
 
-
-    def thread(self, name, app, host, port):
-        serve(app, host=host, port=port)
-
-
-    def __init__(self, client):
-        self.client = client
-
-        """
-        if os.path.exists('topGGSecret.txt'):
-            TopGG.secret = open('topGGSecret.txt', 'r').readline()
-
-            # port 2302 is routed to dev pc, while docker routes 9124->2302
-            _thread.start_new_thread(self.thread, ('flask server', TopGG.app, '0.0.0.0', 2302))
-            print('Started webhook')
-        else:
-            print('Ignoring Webhook, no Token')
-        """
-        
         if os.path.exists('topGGToken.txt'):
+            self.bot = bot
             self.token = open('topGGToken.txt', 'r').readline()
-            self.dblpy = dbl.DBLClient(self.client, self.token, autopost=True)
+
+            #self.dblpy = dbl.DBLClient(self.client, self.token, autopost=True)
+            self.dblpy = dbl.DBLClient(self.bot, self.token)
             print('Started topGG server')
+            self.update_stats.start()
 
         else:
             print('Ignoring TopGG, no Token')
+        
+    def cog_unload(self):
+        self.update_stats.cancel()
 
-
-  
-
-    @commands.Cog.listener()
-    async def on_guild_post(self):
-        print('Server count posted successfully')
-
+    @tasks.loop(minutes=30)
+    async def update_stats(self):
+        """This function runs every 30 minutes to automatically update your server count."""
+        await self.bot.wait_until_ready()
+        try:
+            server_count = len(self.bot.guilds)
+            await self.dblpy.post_guild_count(server_count)
+            print('Posted server count ({})'.format(server_count))
+        except Exception as e:
+            print('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
 
 
 
 def setup(client):
     client.add_cog(TopGG(client))
+
+
