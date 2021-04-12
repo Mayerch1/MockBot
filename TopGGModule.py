3a3b6e10f4342db2db9dc6fc1b723a9f3db09283
Christian <it@cj-mayer.de>
2021-04-12 16:04:54 +0200

    manually post server count

--- TopGGModule.py
+++ TopGGModule.py
@@ -1,41 +1,79 @@
 import os
-import dbl
 import discord
 from discord.ext import commands, tasks
+
+import requests
+import json
 
 
 
 
 class TopGG(commands.Cog):
 
-    def __init__(self, bot):
+    def __init__(self, client):
+        self.BASE = 'https://top.gg/api'
+
+        self.user_agent = "remindMeBot (https://github.com/Mayerch1/RemindmeBot)"
 
         if os.path.exists('topGGToken.txt'):
-            self.bot = bot
+            self.client = client
             self.token = open('topGGToken.txt', 'r').readline()
 
             #self.dblpy = dbl.DBLClient(self.client, self.token, autopost=True)
-            self.dblpy = dbl.DBLClient(self.bot, self.token)
+            #self.dblpy = dbl.DBLClient(self.bot, self.token)
             print('Started topGG server')
             self.update_stats.start()
 
         else:
             print('Ignoring TopGG, no Token')
         
+
     def cog_unload(self):
         self.update_stats.cancel()
+
+    
+
+    async def post_count(self, url, payload):
+        if not self.token:
+            print('no topGGToken')
+            return
+
+        url = self.BASE + url
+        
+        headers = {
+            'User-Agent'   : self.user_agent,
+            'Content-Type' : 'application/json',
+            'Authorization': self.token
+        }
+
+        payload = json.dumps(payload)
+
+        r = requests.post(url, data=payload, headers=headers)
+
+        if r.status_code != 200:
+            print(f'Server Count Post failed with {r.status_code}: {r.content}')
+
+
 
     @tasks.loop(minutes=30)
     async def update_stats(self):
         """This function runs every 30 minutes to automatically update your server count."""
-        await self.bot.wait_until_ready()
-        try:
-            server_count = len(self.bot.guilds)
-            await self.dblpy.post_guild_count(server_count)
-            print('Posted server count ({})'.format(server_count))
-        except Exception as e:
-            print('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
 
+        server_count = len(self.client.guilds)
+
+        payload = {
+            'server_count': server_count
+        }
+        if self.client.shard_count:
+            payload["shard_count"] = self.client.shard_count
+        if self.client.shard_id:
+            payload["shard_id"] = self.client.shard_id
+
+        await self.post_count( f'/bots/{self.client.user.id}/stats', payload=payload)
+
+    @update_stats.before_loop
+    async def update_stats_before(self):
+        await self.client.wait_until_ready()
 
 
 def setup(client):
